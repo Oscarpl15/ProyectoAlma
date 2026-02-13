aee20a91d7a1f9a6d6eeb85cab4dbc6333ffd928
Oscarpl15 <opl1594@gmail.com>
2026-02-13 20:42:28 +0100

    Estructura proyecto creada

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.practicasalma.proyectoalma {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.practicasalma.proyectoalma to javafx.fxml;
+    exports com.practicasalma.proyectoalma;
+}
