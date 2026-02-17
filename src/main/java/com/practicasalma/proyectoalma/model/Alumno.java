a63844447df7d6f4d4a9581d389a453999487cc6
Oscarpl15 <opl1594@gmail.com>
2026-02-17 16:46:01 +0100

    Soluciono problema de tabpanel ligeramente visible

--- src/main/java/com/practicasalma/proyectoalma/model/Alumno.java
+++ src/main/java/com/practicasalma/proyectoalma/model/Alumno.java
@@ -4,6 +4,8 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Alumno {
+
+    //Esto sirve para que se genere una conexion y cuando se modifique el dato, se actualice en la tabla automaticamente.
     private final StringProperty nombre = new SimpleStringProperty();
     private final StringProperty apellidos = new SimpleStringProperty();
     // Añade DNI, Curso, etc.
