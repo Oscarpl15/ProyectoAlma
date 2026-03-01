4d131802bf14fd31e564bd6774f01abaa4517a4d
Oscarpl15 <opl1594@gmail.com>
2026-03-01 11:18:38 +0100

    Añado la clase donación y retoco socio

--- src/main/java/com/practicasalma/proyectoalma/model/Socio.java
+++ src/main/java/com/practicasalma/proyectoalma/model/Socio.java
@@ -2,9 +2,16 @@ package com.practicasalma.proyectoalma.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "socios")
 public class Socio extends Persona {
+
+    // "Física", "Empresa" o "Asociación"
+    @Column(name = "tipo_entidad")
+    private String tipoEntidad;
 
     @Column(name = "cuota")
     private Double cuota;
@@ -13,15 +20,33 @@ public class Socio extends Persona {
     @Column(name = "periodicidad")
     private String periodicidad;
 
+    // Relación Uno a Muchos: Un socio puede tener un historial de muchas donaciones.
+    // mappedBy = "socio" le dice a Hibernate que la relación la controla la clase Donacion.
+    @OneToMany(mappedBy = "socio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Donacion> donaciones = new ArrayList<>();
+
     // Constructor vacío obligatorio para Hibernate
     public Socio() {
         super();
     }
 
-    // Constructor para instanciar desde el controlador JavaFX
-    public Socio(String nombre, String apellidos, String direccion, String dni) {
+    // Constructor para socios ESPORÁDICOS (sin cuota fija ni periodicidad)
+    public Socio(String nombre, String apellidos, String direccion, String dni, String tipoEntidad) {
         super(nombre, apellidos, direccion);
         this.setDni(dni);
+        this.tipoEntidad = tipoEntidad;
+        this.cuota = 0.0; // Lo inicializamos a 0 por defecto
+        this.periodicidad = "Ninguna"; // O null, lo que prefieras manejar en la vista
+    }
+
+    //Constructor para socios con cuota periodica
+    public Socio(String nombre, String apellidos, String direccion, String dni,
+                 String tipoEntidad, Double cuotaFija, String periodicidad) {
+        super(nombre, apellidos, direccion);
+        this.setDni(dni); // El DNI sigue siendo obligatorio
+        this.tipoEntidad = tipoEntidad;
+        this.cuota = cuotaFija;
+        this.periodicidad = periodicidad;
     }
 
     // Getters y Setters
@@ -31,4 +56,17 @@ public class Socio extends Persona {
 
     public String getPeriodicidad() { return periodicidad; }
     public void setPeriodicidad(String periodicidad) { this.periodicidad = periodicidad; }
+
+    //  Métodos de sincronización bidireccional
+
+    public void addDonacion(Donacion donacion) {
+        this.donaciones.add(donacion);
+        // Sincronizamos el otro lado de la relación en memoria
+        donacion.setSocio(this);
+    }
+
+    public void removeDonacion(Donacion donacion) {
+        this.donaciones.remove(donacion);
+        donacion.setSocio(null);
+    }
 }
