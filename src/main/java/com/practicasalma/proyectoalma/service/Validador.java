41a14ec1dbeb44f260bf0898900c6d9e3600c4a0
jaavi_santana <damvespjavisantana@gmail.com>
2026-03-03 08:52:14 +0100

    Funcion esNie() creada

--- src/main/java/com/practicasalma/proyectoalma/service/Validador.java
+++ src/main/java/com/practicasalma/proyectoalma/service/Validador.java
@@ -9,7 +9,7 @@ public class Validador {
             return false;
         }
 
-        dni.trim().toUpperCase();
+        dni = dni.trim().toUpperCase();
 
         if (!dni.matches("^[0-9]{8}[A-Z]$")) {
             return false;
@@ -19,6 +19,33 @@ public class Validador {
         char letraProporcionada = dni.charAt(8);
 
         int numeros = Integer.parseInt(numerosStr);
+        String letrasValidas = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int indice = numeros % 23;
+        char letraCalculada = letrasValidas.charAt(indice);
+
+        return letraProporcionada == letraCalculada;
+    }
+
+    public boolean esNie(String nie) {
+
+        if (nie == null) {
+            return false;
+        }
+
+        nie = nie.trim().toUpperCase();
+
+        if (!nie.matches("^[XYZ][0-9]{7}[A-Z]$")) {
+            return false;
+        }
+
+        String numeroParaCalculo = nie.substring(0, 8);
+        numeroParaCalculo = numeroParaCalculo.replace('X', '0')
+                .replace('Y', '1')
+                .replace('Z', '2');
+
+        char letraProporcionada = nie.charAt(8);
+
+        int numeros = Integer.parseInt(numeroParaCalculo);
         String letrasValidas = "TRWAGMYFPDXBNJZSQVHLCKE";
         int indice = numeros % 23;
         char letraCalculada = letrasValidas.charAt(indice);
